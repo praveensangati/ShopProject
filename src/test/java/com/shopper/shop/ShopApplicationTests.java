0843ed5af5afbaf0292257573b3206c45128f982
User <User@DESKTOP-4L9RCHP>
2020-07-09 18:56:39 +0530

    initial commit

---
+++ src/test/java/com/shopper/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopper.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
