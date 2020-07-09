4199dfabc19d3a4cd745ed96244e656315f573bb
User <User@DESKTOP-4L9RCHP>
2020-07-09 19:02:05 +0530

    commit test

--- src/main/java/com/shopper/shop/ShopApplication.java
+++ src/main/java/com/shopper/shop/ShopApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopApplication {
 
+	//intial project
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
